7f3e7a133138833c7ecc2a0698bd1ad1c60986ec
jake-gu <gu.jack@gmail.com>
2026-04-22 14:48:51 -0700

    Update app setup and logger

--- src/app.js
+++ src/app.js
@@ -1,8 +1,44 @@
 import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';  
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import logger from './utils/logger.js';
+import authRoutes from './routes/auth.routes.js'; 
+import 'dotenv/config';
+import winston from 'winston';
+
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(
+  morgan('combined', {
+    stream: {
+      write: (message) => logger.info(message.trim()),
+    },
+  })
+);
+
 app.get('/', (req, res) => {
-  res.status(200).send('Hello from acquisitionsAPI!');
+  logger.info('Hello from Acquisitions!');
+  res.status(200).send('Hello from Acquisitions!');
 });
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok', timestamp: new Date().toISOString() , uptime: process.uptime() });
+});
+
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the Acquisitions API!' });
+});
+
+
+
+app.use('/api/auth', authRoutes);
 
 export default app;
